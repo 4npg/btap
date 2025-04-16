a439c0f65296396749b6a1ee738457af2e6e0535
dpl107 <hedgehog7421@gmail.com>
2025-04-16 17:11:47 +0700

    first dfs task

--- nhap.cpp
+++ nhap.cpp
@@ -1,35 +1,47 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
+
 using namespace std;
-//n <= 1000 ==> int
-bool nto(int n){
-    if(n<2)return false;
-    for(int i = 2;i*i<=n;i++)
-        if(n%i==0)return false;
-    return true;
-}
-int main(){
-    ios_base::sync_with_stdio(false);cin.tie(nullptr);
-    int n;cin>>n;
-    set<int> unto;
-    set<int> ut;
-    for(int i=1;i*i<=n;i++){
-        if(n%i==0){
-            if(nto(i))unto.insert(i);
-            ut.insert(i);
-            if(n/i!=i){
-                if(nto(n/i))unto.insert(n/i);
-                ut.insert(n/i);
+
+const int maxN = 10010;
+
+int n, m;
+bool joint[maxN];
+int timeDfs = 0, bridge = 0;
+int low[maxN], num[maxN];
+vector <int> g[maxN];
+
+void dfs(int u, int pre) {
+    int child = 0; // Số lượng con trực tiếp của đỉnh u trong cây DFS
+    num[u] = low[u] = ++timeDfs;
+    for (int v : g[u]) {
+        if (v == pre) continue;
+        if (!num[v]) {
+            dfs(v, u);
+            low[u] = min(low[u], low[v]);
+            if (low[v] == num[v]) bridge++;
+            child++;
+            if (u == pre) { // Nếu u là đỉnh gốc của cây DFS
+                if (child > 1) joint[u] = true;
             }
+            else if (low[v] >= num[u]) joint[u] = true;
         }
+        else low[u] = min(low[u], num[v]);
     }
-    for(auto x:ut){
-        cout<<x<<" ";
+}
+
+int main() {
+    cin >> n >> m;
+    for (int i = 1; i <= m; i++) {
+        int u, v;
+        cin >> u >> v;
+        g[u].push_back(v);
+        g[v].push_back(u);
     }
-    cout<<"\n";
-    if(unto.size()==0){
-        cout<<0;
-    }else{
-    for(auto x:unto){
-        cout<<x<<" ";
-    }}
+    for (int i = 1; i <= n; i++)
+        if (!num[i]) dfs(i, i);
+
+    int cntJoint = 0;
+    for (int i = 1; i <= n; i++) cntJoint += joint[i];
+
+    cout <<"so khop la: "<< cntJoint <<"\nso canh cau la: " << bridge;
 }
