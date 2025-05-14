631e376b7f41c47ebf7e787106da083845c5358f
l1lbgdv <hedgehog7421@gmail.com>
2025-05-14 12:59:28 +0700

    14/05/2025

--- nhap.cpp
+++ nhap.cpp
@@ -1,26 +1,30 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
+#define ll long long
+#define fast ios_base::sync_with_stdio(false);cin.tie(nullptr)
 
-int main() {
-    // Ví dụ: Một map tên -> tuổi
-    map<string,int> age = {
-        {"Alice", 30},
-        {"Bob",   25},
-        {"Carol", 28}
-    };
-
-    cout << "=== Dùng Structured Binding (C++17) ===\n";
-    for (auto [name, years] : age) {
-        // name  là khóa (first), years là giá trị (second)
-        cout << name << " is " << years << " years old.\n";
-    }
-
-    cout << "\n=== Dùng cách truy xuất truyền thống ===\n";
-    for (const auto &p : age) {
-        const string &name = p.first;
-        int years         = p.second;
-        cout << name << " is " << years << " years old.\n";
-    }
-
-    return 0;
+bool nto(ll n){
+    if(n<2)return false;
+    for(ll i=2;i*i<=n;i++)if(n%i==0)return false;
+        return true;
 }
+bool xuli(ll n){
+    ll s=0;
+    if(!nto(n))return false;
+    else{
+        while(n){
+            s+=n%10;
+            if(n%10!=2&&n%10!=3&&n%10!=5&&n%10!=7)return false;
+            n/=10;
+        }
+    }
+    if(!nto(s))return false;
+    return true;
+}
+int main(){
+    fast;
+    ll a,b;cin>>a>>b;
+    ll cnt=0;
+    for(ll i=a;i<=b;i++)if(xuli(i)&&nto(i))cnt++;
+        cout<<cnt;
+} 
