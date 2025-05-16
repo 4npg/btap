ebab5b1e755f05ca37a3ce1efbc1ffe232f2a43e
l1lbgdv <hedgehog7421@gmail.com>
2025-05-17 00:31:13 +0700

    cc cut

--- linh tinh/nhap.cpp
+++ linh tinh/nhap.cpp
@@ -1,30 +1,27 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 #define ll long long
-#define fast ios_base::sync_with_stdio(false);cin.tie(nullptr)
+#define TASK "tenbai"
+#define endl "\n"
+#define fast ios_base::sync_with_stdio(false); cin.tie(nullptr)
+#define FOR(i,a,b) for(ll (i)=(a);i<=(b);++i)
+#define LOCAL
 
-bool nto(ll n){
-    if(n<2)return false;
-    for(ll i=2;i*i<=n;i++)if(n%i==0)return false;
-        return true;
-}
-bool xuli(ll n){
-    ll s=0;
-    if(!nto(n))return false;
-    else{
-        while(n){
-            s+=n%10;
-            if(n%10!=2&&n%10!=3&&n%10!=5&&n%10!=7)return false;
-            n/=10;
-        }
-    }
-    if(!nto(s))return false;
-    return true;
-}
-int main(){
+#ifdef LOCAL
+  #define dbg(x) do { cerr << "[" << #x << "] = " << (x) << '\n'; } while(0)
+#else
+  #define dbg(x) do {} while(0)
+#endif
+
+
+int32_t main() {
     fast;
-    ll a,b;cin>>a>>b;
-    ll cnt=0;
-    for(ll i=a;i<=b;i++)if(xuli(i)&&nto(i))cnt++;
-        cout<<cnt;
-} 
+    if (fopen(TASK ".inp", "r")) {
+        freopen(TASK ".inp", "r", stdin);
+        freopen(TASK ".out", "w", stdout);
+    }
+
+
+}
+
+
