b2004c16caa3e7e23b03852c33b8deb21d076911
l1lbgdv <hedgehog7421@gmail.com>
2025-05-24 01:49:32 +0700

    fix sotanghoacgiam

--- linh tinh/ts.cpp
+++ linh tinh/ts.cpp
@@ -2,8 +2,6 @@
 using namespace std;
 #define ll long long
 #define TASK "tenbai"
-#define pb push_back
-#define ALL(a) (a).begin(),(a).end()
 #define endl "\n"
 #define fast ios_base::sync_with_stdio(false); cin.tie(nullptr)
 #define FOR(i,a,b) for(ll (i)=(a);i<=(b);++i)
@@ -15,63 +13,48 @@ using namespace std;
   #define dbg(x) do {} while(0)
 #endif
 
-const ll maxn =1e7;
-
-ll d[maxn];
-void sang(){
-    d[0]=d[1]=1;
-    for(ll i=2;i*i<maxn;i++){
-        if(d[i]==0)for(ll j=i*i;j<maxn;j+=i)d[j]=1;
+ll ipow(ll base, ll exp) {
+    ll result = 1;
+    while (exp > 0) {
+        if (exp & 1)            // nếu bit thấp nhất của exp là 1
+            result = result * base;
+        base = base * base;     // bình phương cơ số
+        exp >>= 1;              // dịch exp sang phải 1 bit
     }
+    return result;
 }
-bool xuli(ll n){
-    vector<ll>dg;
-    while(n){
-        dg.pb(n%10);n/=10;
+bool tangorgiam(const string &s){
+    bool inc = true, dec = true;
+    for(int i = 1; i < (int)s.size(); ++i){
+        if(s[i] <= s[i-1]) inc = false;
+        if(s[i] >= s[i-1]) dec = false;
     }
-    reverse(ALL(dg));
-    if(dg.size()<2)return false;
-    if(dg.size()==2){
-        return dg[0]!=dg[1];
-    }
-    ll huong = dg[1]-dg[0];
-    if(huong==0)return false;
-    FOR(i,2,dg.size()-1){
-        ll dif = dg[i]-dg[i-1];
-        if(dif==0||dif*huong>0)return false;
-        huong=dif;
-    }
+    return inc || dec;
+}
+bool nto(ll n){
+    if(n<2)return false;
+    if(n==2||n==3)return true;
+    if(n%2==0||n%3==0)return false;
+    for(ll i=5;i*i<=n;i+=6)if(n%i==0||n%(i+2)==0)return false;
     return true;
 }
-bool xuli2(ll n){
-    string str = to_string(n);
-    if(str.size()<2)return false;
-    if(str.size()==2)return (bool)(str[0]!=str[1]);
-    ll huong = (ll)(str[1]-str[0]);
-    if(huong==0)return false;
-    FOR(i,2,str.size()-1){
-        ll dif = (ll)(str[i]-str[i-1]);
-        if(dif==0||dif*huong>0)return false;
-        huong=dif;
+ll cnt(ll st,ll ed){
+    ll cn=0;
+    FOR(i,st,ed){
+        string str = to_string(i);
+        cn+=((tangorgiam(str)&&nto(i))?1:0);
     }
-    return true;
+    return cn;
 }
 int32_t main() {
     fast;
-     
-    ll n;cin>>n;ll cnt=0;
-    sang();
-    while(n--){
-        ll x;cin>>x;
-        if(d[x]==0){
-            if(xuli(x)){
-                cnt++;
-                cout<<x<<" ";
-            }
-        }
+    if (fopen(TASK ".inp", "r")) {
+        freopen(TASK ".inp", "r", stdin);
+        freopen(TASK ".out", "w", stdout);
     }
-    cout<<endl<<cnt;
-
+    ll n;cin>>n;
+    ll limit = ipow(10,n)-1,st = ipow(10,n)/10;
+    cout<<cnt(st,limit);
 }
 
 
