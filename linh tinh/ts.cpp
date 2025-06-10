de4d032a827b52ab4f8683d98d39841119ef29bf
l1lbgdv <hedgehog7421@gmail.com>
2025-06-10 17:02:42 +0700

    de 7

--- linh tinh/ts.cpp
+++ linh tinh/ts.cpp
@@ -1,7 +1,38 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-int main(){
-    int b = 5;
-    int c = 2;
-    cout<< b+c<<" "<<b-c; 
+#define ll long long
+#define TASK "tenbai"
+#define endl "\n"
+#define fast ios_base::sync_with_stdio(false); cin.tie(nullptr)
+#define FOR(i,a,b) for(ll (i)=(a);i<=(b);++i)
+#define LOCAL
+
+#ifdef LOCAL
+  #define dbg(x) do { cerr << "[" << #x << "] = " << (x) << '\n'; } while(0)
+#else
+  #define dbg(x) do {} while(0)
+#endif
+
+
+bool prime(ll n){
+    if(n<2)return false;
+    if(n==2||n==3)return true;
+    if(n%2==0||n%3==0)return false;
+    for(ll i=5;i*i<=n;i+=6){
+        if(n%i==0||n%(i+2)==0)return false;
+    }
+    return true;
 }
+
+
+int32_t main() {
+    fast;
+    if (fopen(TASK ".inp", "r")) {
+        freopen(TASK ".inp", "r", stdin);
+        freopen(TASK ".out", "w", stdout);
+    }
+    cout<<((prime(2023)?"PRIME":"NOT PRIME"));
+
+}
+
+
