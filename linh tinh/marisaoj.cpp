fbd1dd0e811e77351f453cddc22fae3334a21f0b
l1lbgdv <hedgehog7421@gmail.com>
2025-07-31 00:01:29 +0700

    30/07/2025

--- linh tinh/marisaoj.cpp
+++ linh tinh/marisaoj.cpp
@@ -1,11 +1,13 @@
-// author: anphung iu bcu
+// authur : anphung
+// github : 4npg
 #include <bits/stdc++.h>
 using namespace std;
 #define int64 long long
 #define TASK "tenbai"
-#define el cout << "\n"
+#define el cout<<"\n"
 #define fast ios_base::sync_with_stdio(false); cin.tie(nullptr)
 #define FOR(i,a,b) for(int (i)=(a);i<=(b);++i)
+
 
 int32_t main() {
     fast;
@@ -13,22 +15,8 @@ int32_t main() {
         freopen(TASK ".inp", "r", stdin);
         freopen(TASK ".out", "w", stdout);
     }
-    string s;
-    getline(cin, s);
-    int k; cin >> k;
-    string ans ="";
-    for (auto &c : s) {
-        if (c != ' ') {
-            char rotated;
-            if (islower(c)) rotated = 'a' + (c - 'a' + k) % 26;
-            else if (isupper(c)) rotated = 'A' + (c - 'A' + k) % 26;
-            else rotated = c;
-            cout<<rotated;
-        }else
-        cout<<c;
-    }
-    // 
-    // el;
-    // cout << s;
-    // 
+
+
 }
+
+
